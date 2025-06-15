e5d3a17a81c404a1d7d15005da3693d33ebe635d
dana259 <dana.boada@udla.edu.ec>
2025-06-15 17:03:58 -0500

    Agregé la carpeta Viewa y en la misma puse el elemento AllNotesPage

--- BoadaDApuntes/Models/About.cs
+++ BoadaDApuntes/Models/About.cs
@@ -5,7 +5,6 @@ internal class About
     public string Title => AppInfo.Name;
     public string Version => AppInfo.VersionString;
     public string MoreInfoUrl => "https://aka.ms/maui";
-    public string Message => "Esta app está escrita en XAML y C# con .NET MAUI por Dana Boada.";
     public string Name => "Dana Boada";
     public string Description => "Estudiante de Ingeniería de Software";
     public string Hobbies => "Mis hobbies incluyen tocar el violín, el piano y escuchar música";
