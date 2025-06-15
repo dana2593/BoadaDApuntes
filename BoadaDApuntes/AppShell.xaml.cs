2a499e30c38ea74fc8113a186c4ac18af7b604bf
dana259 <dana.boada@udla.edu.ec>
2025-06-15 16:24:29 -0500

    Agregar archivos de proyecto.

---
+++ BoadaDApuntes/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BoadaDApuntes
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BoadaDApuntes/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BoadaDApuntes
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
