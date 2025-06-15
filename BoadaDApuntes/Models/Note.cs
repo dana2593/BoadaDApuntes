5a67ad70e473dc0cde46434a6c285aeb2d5ca8a2
dana259 <dana.boada@udla.edu.ec>
2025-06-15 16:51:58 -0500

    Se creó la carpeta Models y en ella la clase Note

---
+++ BoadaDApuntes/Models/Note.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoadaDApuntes.Models;
+
+internal class Note
+{
+    public string Filename { get; set; } = string.Empty;
+    public string Text { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+}
